0d71a39ef47a07f0690532471772910349794327
totewiwittaporn <totee.wiwittaporn@gmail.com>
2025-10-08 18:27:58 +0700

    delete sidebar

--- client/src/components/layout/AppShell.jsx
+++ client/src/components/layout/AppShell.jsx
@@ -1,8 +1,7 @@
 export default function AppShell({ sidebar, children }) {
-return (
-<div className="container-app py-6 grid grid-cols-1 md:grid-cols-[14rem_1fr] gap-6">
-{sidebar}
-<div className="grid gap-6">{children}</div>
-</div>
-);
+  return (
+    <div className="container-app py-6">
+      <div>{children}</div>
+    </div>
+  );
 }
