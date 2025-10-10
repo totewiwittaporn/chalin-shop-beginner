1d45cd6a925efa2f3a703693226a07b475ab77f5
totewiwittaporn <totee.wiwittaporn@gmail.com>
2025-10-10 11:29:22 +0700

    backup before routes->pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'] // บังคับให้ใช้ React ชุดเดียว
-  }
+  base: '/chalin-shop-beginner/', // ใช้ชื่อ repo ของคุณ
+      dedupe: ['react', 'react-dom'] // บังคับให้ใช้ React ชุดเดียว
+
 })
